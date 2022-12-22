fcab0181c8b3a10195c74174e9e5c345a87b3a30
Asher_CDV3_p <asherferri85@gmail.com>
2022-12-22 00:02:21 -0500

    second commit lol

--- notepad_notes.py
+++ notepad_notes.py
@@ -3,10 +3,36 @@ import os
 from tkinter import *
 from tkinter.messagebox import *
 from tkinter.filedialog import *
-
 class Notepad:
     __root = Tk()
     __thisWidth = 300
     __thisHeight = 300
     __thisTextArea = Text(__root)
+    __thisMenuBar = Menu(__root)
+    __thisFileMenu = Menu(__thisMenuBar, tearoff=0)
+    __thisEditMenu = Menu(__thisMenuBar, tearoff=0)
+    __thisHelpMenu = Menu(__thisMenuBar, tearoff=0)
+
+__thisScrollBar = Scrollbar(__thisTextArea)
+__file = None
+def __init__(self, **kwargs)
+
+    try:
+        self.__root.wm_iconbitmap('Notepad.ico')
+    except:
+        pass
+
+    try:
+        self.__thisWidth = kwargs['width']
+    except KeyError:
+        pass
+    try:
+        self.__thisHeight = kwargs['height']
+    except KeyError:
+        pass
+
+    self.__root.title("Untitled - Notepad")
+
+    screenWidth = self.__root.winfo_screenwidth()
+    screenHeight = self._root.winfo_screenheight()
     
